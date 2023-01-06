ce6292e703768a8010cdfbed323fd0c1378b3e17
rabia.gultekin <rabia.gultekin@sahinbey.bel.tr>
2023-01-06 16:57:16 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import { Col, Container, Row } from "reactstrap";
+import CategoryList from "./CategoryList";
+import Navi from "./Navi";
+import ProductList from "./ProductList";
+
+function App() {
+  let productInfo = {title:"Product List Here"}
+  let categoryInfo = {title:"Category List Here"}
+  return (
+    <div>
+      <Container>
+        <Row>
+          <Navi>aaaa</Navi>
+        </Row>
+        <Row>
+          <Col xs="3">
+            <CategoryList info={categoryInfo} />
+          </Col>
+          <Col xs="9">
+            <ProductList info={productInfo} />
+          </Col>
+        </Row>
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Navi.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+        <h2>navi component</h2>
+        </div>
+      </div>
+    )
+  }
+}
